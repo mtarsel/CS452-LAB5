676d08ee7ae3ef66449d262281b0cb65d2f3a620
Mick Tarsel <mtarsel@gmail.com>
2014-03-31 21:50:22 -0400

    cleaning up bad code

--- lab5.cpp
+++ lab5.cpp
@@ -2,26 +2,17 @@
 #include <string.h>
 #include "GL/glut.h"
 
-
 #define TIMER 33
 
 GLUquadric *myQuad;
 
-
 static float xrot;
 
-#define TMMODE_NONE 1
-#define TMMODE_SINGLE 2
-#define TMMODE_TWOPASS 3
-#ifdef GL_HP_texture_lighting
-#define TMMODE_HPEXT 4
-#endif /* GL_HP_texture_lighting */
 static int tmMode;
 #define USE_ALPHA_TEST 10
 static int alphaTest = 0;
 
 static GLuint textures[2];
-
 
 static void display( void )
 {
@@ -37,25 +28,6 @@ static void display( void )
 
     /* Only pass or first of two passes */
     gluCylinder (myQuad, .5, .5, 2., 24, 8);
-    
-    if (tmMode == TMMODE_TWOPASS) {
-        GLfloat white[4] = {1., 1., 1., 1.};
-
-        /* second pass here */
-        glPushAttrib (GL_ALL_ATTRIB_BITS);
-        glColor3f (0., 0., 0.);
-        glDepthFunc (GL_LEQUAL);
-        glEnable (GL_BLEND);
-        glLightfv (GL_LIGHT1, GL_SPECULAR, white);
-        glBindTexture (GL_TEXTURE_2D, textures[1]);
-        
-        gluCylinder (myQuad, .5, .5, 2., 24, 8);
-        
-        glPopAttrib ();
-
-        /* Bug! Should have popped... */
-        glBindTexture (GL_TEXTURE_2D, textures[0]);
-    }
 
     glFlush();
     glutSwapBuffers();
@@ -72,25 +44,6 @@ void reshape(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
-static void cbMainMenu (int value)
-{
-    GLfloat white[4] = {1., 1., 1., 1.};
-    GLfloat black[4] = {0., 0., 0., 0.};
-
-    
-    if (value == 99) {
-        exit (0);
-    }
-
-    if (value < USE_ALPHA_TEST)
-        tmMode = value;
-
-        glEnable (GL_TEXTURE_2D);
-        glLightfv (GL_LIGHT1, GL_SPECULAR, white);
-#ifdef GL_HP_texture_lighting
-        glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_LIGHTING_MODE_HP, GL_TEXTURE_POST_SPECULAR_HP);
-#endif /* GL_HP_texture_lighting */
-}
 
 static void timer (int value)
 {
@@ -121,9 +74,11 @@ static void init ()
 {
     int mainMenu;
     GLfloat pos[4] = {3., 5., 2., 1.};
+    /*for the checkerboard */
     GLfloat white[4] = {1., 1., 1., 1.};
+    GLfloat black[4] = {0., 0., 0., 0.};
     
-    xrot = 0.;
+    xrot = 0;
 
     glClearColor (.3, .3, .3, 0.);
     glColor4f (.5, .5, .5, 1.); /* cylinder is grey by default */
@@ -132,22 +87,6 @@ static void init ()
 
     /* Set up alpha test to toss 0 alpha */
     glAlphaFunc (GL_NOTEQUAL, 0);
-
-    /* blending function used in TMMODE_TWOPASS */
-    glBlendFunc (GL_ONE, GL_ONE);
-
-    /* Set up light1 */
-    glEnable (GL_LIGHTING);
-    glEnable (GL_LIGHT1);
-    glLightfv (GL_LIGHT1, GL_POSITION, pos);
-    glLightfv (GL_LIGHT1, GL_DIFFUSE, white);
-    glLightfv (GL_LIGHT1, GL_SPECULAR, white);
-
-    /* ambient and diffuse will track glColor */
-    glEnable (GL_COLOR_MATERIAL);
-    glColorMaterial (GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    glMaterialf (GL_FRONT, GL_SHININESS, 30.f);
-    glMaterialfv (GL_FRONT, GL_SPECULAR, white);
     
     /* Set up texture coord generation */
     {
@@ -163,7 +102,7 @@ static void init ()
     /* prepare to use two texture objects */
     glGenTextures (2, textures);
     
-    /* init first texture object, a red & black checkerboard, ho-hum */
+    /* init first texture object, a red & black checkerboard */
     glBindTexture (GL_TEXTURE_2D, textures[0]);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -177,18 +116,6 @@ static void init ()
                   0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
         free (pixels);
     }
-
-    /* Second texture is a 2x2 white texture map for the sdpecular pass of TMMODE_TWOPASS */
-    glBindTexture (GL_TEXTURE_2D, textures[1]);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    {
-        unsigned char pixels[2*2*4];
-        memset (pixels, 0xff, 2*2*4);
-        glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, 2, 2,
-                  0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-    }
     
     /* Leave first texture bound */
     glBindTexture (GL_TEXTURE_2D, textures[0]);
@@ -200,9 +127,6 @@ static void init ()
     glutTimerFunc (TIMER, timer, 0);
 
     int value;
-   
-    GLfloat black[4] = {0., 0., 0., 0.};
-
 
     if (value == 99) {
         exit (0);
@@ -213,24 +137,8 @@ static void init ()
 
         glEnable (GL_TEXTURE_2D);
         glLightfv (GL_LIGHT1, GL_SPECULAR, white);
-#ifdef GL_HP_texture_lighting
-        glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_LIGHTING_MODE_HP,
-GL_TEXTURE_POST_SPECULAR_HP);
-#endif /* GL_HP_texture_lighting */
 
-
- 
-//    mainMenu = glutCreateMenu (cbMainMenu);
-    glutAddMenuEntry ("No texture mapping", TMMODE_NONE);
-    glutAddMenuEntry ("Single pass", TMMODE_SINGLE);
-    glutAddMenuEntry ("Two pass", TMMODE_TWOPASS);
-#ifdef GL_HP_texture_lighting
-    glutAddMenuEntry ("HP pre-specular", TMMODE_HPEXT);
-#endif /* GL_HP_texture_lighting */
-    glutAddMenuEntry ("Toggle alpha test", USE_ALPHA_TEST);
-    glutAddMenuEntry ("Quit", 99);
-    glutAttachMenu (GLUT_RIGHT_BUTTON);
-    tmMode = TMMODE_NONE;
+        glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_LIGHTING_MODE_HP, GL_TEXTURE_POST_SPECULAR_HP);
 }
 
 int main(int argc, char** argv)
@@ -240,7 +148,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE); 
     glutInitWindowSize (300,300);
     glutInitWindowPosition (0,0); 
-    glutCreateWindow ("specular lighting and texture mapping");
+    glutCreateWindow ("Lab 5");
 
     init ();
 
